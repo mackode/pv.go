60574270f63536d4835e72dcb71b5ffc11f505bd
Bartek Makowski <biuro@mackode.pl>
2024-08-26 12:33:30 +0000

    Add string masking utils

---
+++ util.go
@@ -0,0 +1,20 @@
+package main
+
+func masks(s string) string {
+  masked := []byte(s)
+  tomask := false
+
+  for i := 0; i < len(s); i++ {
+    if tomask {
+      masked[i] = '*'
+    } else {
+      masked[i] = s[i]
+    }
+
+    if s[i] == '' {
+      tomask = true
+    }
+  }
+
+  return string(masked)
+}
